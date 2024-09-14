ca42f1be651b16283d3ce5d98208e9191e6a8538
Shubhanshu Dwivedi <129971064+Shubhanshu5571@users.noreply.github.com>
2024-09-14 18:22:31 +0530

    Update text-to-speech.py

--- text-to-speech.py
+++ text-to-speech.py
@@ -1,8 +1,8 @@
 
 
+from gtts import gTTS
 import streamlit as st
 
-import pyttsx3
 
 st.title('Text to speech App')
 
@@ -10,50 +10,17 @@ st.write('It is a text to speech app so you enter text in below prompt and after
 
 user_input= st.text_area('Enter text here')
 
+if st.button("Convert to Speech"):
+    if user_input:
+         
+        # Convert text to speech
+        tts = gTTS(user_input, lang='en')
 
-if st.button('Male Voice'):
-    #text_to_speech = gTTS(user_input)
-    #text_to_speech.save('text_to_speech_gtts.wav')
-    #sound_file = 'text_to_speech_gtts.wav'
-    #st.audio(sound_file,autoplay=True)
-    audio = pyttsx3.init(driverName='sapi5')
-    audio.setProperty('rate', 130)
-    audio.setProperty('volume', 1.0)
+        # Save the converted audio to a file
+        tts.save("text_to_speech_gtts.wav")
 
-# change the voices
-    voice = audio.getProperty('voices')
-
-# 0 for male and 1 for female
-    audio.setProperty('voice', voice[0].id)      # for male voice   
-#audio.setProperty('voice', voice[1].id)       # for female voice
-
-# text-to speech conversion
-    audio.say(user_input)
-
-# save the audio file
-    audio.save_to_file(user_input, 'test_male_Voice.mp3')
-    #audio.save_to_file(user_input, 'test_female_Voice.mp3')
-
-    audio.runAndWait()
-    
-if st.button('Female voice'):
-    
-    audio = pyttsx3.init(driverName='sapi5')
-    audio.setProperty('rate', 150)
-    audio.setProperty('volume', 1.0)
-
-# change the voices
-    voice = audio.getProperty('voices')
-
-# 0 for male and 1 for female
-    #audio.setProperty('voice', voice[0].id)      # for male voice   
-    audio.setProperty('voice', voice[1].id)       # for female voice
-
-# text-to speech conversion
-    audio.say(user_input)
-
-# save the audio file
-    #audio.save_to_file(user_input, 'test_male_Voice.mp3')
-    audio.save_to_file(user_input, 'test_female_Voice.mp3')
-
-    audio.runAndWait()
+        # Play the audio file in the Streamlit app
+        audio_file = 'text_to_speech_gtts.wav'
+        st.audio(audio_file, format="audio/wav",autoplay=True)
+    else:
+        st.warning("Please enter some text.")
