dd0103f1a3106ae534e16e71b9f187ecc5153565
Shubhanshu Dwivedi <129971064+Shubhanshu5571@users.noreply.github.com>
2024-09-14 15:19:43 +0530

    Add files via upload

---
+++ text-to-speech.py
@@ -0,0 +1,59 @@
+
+
+import streamlit as st
+#from gtts import gTTS
+import pyttsx3
+
+st.title('Text to speech App')
+
+st.write('It is a text to speech app so you enter text in below prompt and after enter text press button you listen this')
+
+user_input= st.text_area('Enter text here')
+
+
+if st.button('Male Voice'):
+    #text_to_speech = gTTS(user_input)
+    #text_to_speech.save('text_to_speech_gtts.wav')
+    #sound_file = 'text_to_speech_gtts.wav'
+    #st.audio(sound_file,autoplay=True)
+    audio = pyttsx3.init()
+    audio.setProperty('rate', 130)
+    audio.setProperty('volume', 1.0)
+
+# change the voices
+    voice = audio.getProperty('voices')
+
+# 0 for male and 1 for female
+    audio.setProperty('voice', voice[0].id)      # for male voice   
+#audio.setProperty('voice', voice[1].id)       # for female voice
+
+# text-to speech conversion
+    audio.say(user_input)
+
+# save the audio file
+    audio.save_to_file(user_input, 'test_male_Voice.mp3')
+    #audio.save_to_file(user_input, 'test_female_Voice.mp3')
+
+    audio.runAndWait()
+    
+if st.button('Female voice'):
+    
+    audio = pyttsx3.init()
+    audio.setProperty('rate', 150)
+    audio.setProperty('volume', 1.0)
+
+# change the voices
+    voice = audio.getProperty('voices')
+
+# 0 for male and 1 for female
+    #audio.setProperty('voice', voice[0].id)      # for male voice   
+    audio.setProperty('voice', voice[1].id)       # for female voice
+
+# text-to speech conversion
+    audio.say(user_input)
+
+# save the audio file
+    #audio.save_to_file(user_input, 'test_male_Voice.mp3')
+    audio.save_to_file(user_input, 'test_female_Voice.mp3')
+
+    audio.runAndWait()
